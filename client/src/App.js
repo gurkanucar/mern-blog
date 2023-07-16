e78ebe6861b737d8fee28f79b21f17727afd6432
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-07-16 10:15:41 +0300

    refactor: css updates

--- client/src/App.js
+++ client/src/App.js
@@ -14,8 +14,14 @@ function App() {
         <div className="image">
           <img src="https://techcrunch.com/wp-content/uploads/2023/07/GettyImages-500665269.jpg?w=430&h=230&crop=1" />
         </div>
+
         <div className="texts">
           <h2>On-prem data centers are hanging in, but cloud capacity is growing much faster</h2>
+          <div className="info">
+            <a className="author" href="">Gurkan UCAR</a>
+            <time>2023-07-18 9:39 AM</time>
+
+          </div>
           <p>In 2017, on-prem data centers accounted for nearly 60% of capacity. By 2027, Synergy Research is projecting that number will be cut in half, but that data doesn’t tell the whole story. New research from Synergy finds that on-prem data center growth is actually hanging in, but over the next five years, growth will remain essentially unchanged. At the same time, hyperscale data centers operated by the largest cloud companies will continue to grow at a hefty rate.</p>
         </div>
       </div>
