8889ae31a935195983d65199390e5f71d79eb197
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-07-16 12:46:47 +0300

    feat: HomePage cerated

---
+++ client/src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Entry from '../../components/Entry/Entry'
+
+const HomePage = () => {
+  return (
+    <div>
+        <Entry />
+        <Entry />
+        <Entry />
+    </div>
+  )
+}
+
+export default HomePage
