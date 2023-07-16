8131126a9ff24b76a11a0228282f474bd097b8a4
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-07-16 19:44:25 +0300

    refactor: register user api update

--- api/index.js
+++ api/index.js
@@ -1,40 +1,42 @@
+require('dotenv').config();
 const express = require("express")
 const cors = require("cors");
+const mongoose = require("mongoose");
+
+const User = require('./models/User');
 
 var corsOptions = {
     origin: "*",
 };
+
+mongoose.connect(process.env.MONGO_CONNECTION_URL)
 
 const app = express()
 app.use(express.json())
 app.use(cors(corsOptions));
 
 
-var users = []
 
 app.post("/login", (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
-    console.log(users)
-    if (users.filter(x => x.username == username && x.password == password).length > 0) {
-        res.json({
-            "message": "success"
-        })
-    } else {
-
-        res.status(404).send({
-            "message": "user not found"
-        })
-    }
-});
-
-
-app.post("/register", (req, res) => {
-    console.log(req.body)
-    users.push(req.body)
     res.json({
         "message": "success"
     })
 });
 
+
+app.post("/register", async (req, res) => {
+    console.log(req.body)
+    const { username, email, password } = req.body;
+    const userDoc = await User.create({
+        username, email, password
+    });
+    res.json({
+        "message": "success",
+        "user": userDoc
+    })
+});
+
 app.listen(8080);
+
