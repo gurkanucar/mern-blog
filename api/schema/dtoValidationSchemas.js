920802fed61a87da56aa8416252567f117c67364
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-07-20 01:13:41 +0300

    feat: validationSchemas file created

---
+++ api/schema/dtoValidationSchemas.js
@@ -0,0 +1,11 @@
+const Joi = require("joi");
+
+const registerSchema = Joi.object({
+    username: Joi.string().min(4).max(12).required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().min(6).required(),
+}).options({ abortEarly: false });
+
+module.exports = {
+    registerSchema,
+};
