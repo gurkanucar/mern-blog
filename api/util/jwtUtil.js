40ccf7a9941a6a06a1bd95b99c4dc5eec07ab702
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-07-22 12:17:03 +0300

    feat: validateToken method added to jwt utils

--- api/util/jwtUtil.js
+++ api/util/jwtUtil.js
@@ -1,10 +1,21 @@
 const jwt = require("jsonwebtoken");
+const UnAuthorizedError = require("../errors/unAuthorizedError");
 const jwt_secret = "myjwtsecret";
 
+
+const validateToken = (token) => {
+  try {
+    const decodedToken = jwt.verify(token, jwt_secret);
+    return decodedToken;
+  } catch (err) {
+    throw new UnAuthorizedError("Invalid token!");
+  }
+};
+
 const generateToken = (payload) => {
-  return jwt.sign(payload, jwt_secret, {});
+  return jwt.sign(payload, jwt_secret, { expiresIn: "20h" });
 };
 
 module.exports = {
-  generateToken,
+  generateToken, validateToken
 };
